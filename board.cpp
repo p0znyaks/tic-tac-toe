2ba712660ff8f41ddee86d4001cc415ef9a222fc
p0znYaks <fdsfsfd@fdsf.com>
2024-01-08 16:45:00 +0300

    added board.cpp

---
+++ board.cpp
@@ -0,0 +1,19 @@
+#include "Header.h"
+
+extern char square[10];
+
+void board()
+{
+    cout << "\n\n\tКрестики-нолики\n\n";
+    cout << "Игрок 1 (X)  -  Игрок 2 (O)" << endl << endl;
+    cout << endl;
+    cout << "     |     |     " << endl;
+    cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << endl;
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+    cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << endl;
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+    cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << endl;
+    cout << "     |     |     " << endl << endl;
+}
