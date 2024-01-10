11b4ad6db13eb25a353b835c0c2293a841cda5ce
Pavlovich Ilya <155195533+dealife@users.noreply.github.com>
2024-01-10 16:04:40 +0300

    Update winrate.cpp

--- winrate.cpp
+++ winrate.cpp
@@ -1,40 +1,26 @@
-﻿#include <iostream>
+
 #include "Header.h"
-	void writeIntToFile(int number, const std::string& filename) {
-		std::ofstream file(filename, std::ios::trunc);
-		if (file.is_open()) {
-			file << number;
-			file.close();
-			std::cout << "Число успешно записано в файл " << filename << std::endl;
-		}
-		else {
-			std::cout << "Не удалось открыть файл " << filename << std::endl;
-		}
+void writeIntToFile(int number, const std::string& filename) {
+	std::ofstream file(filename, std::ios::trunc);
+	if (file.is_open()) {
+		file << number;
+		file.close();
 	}
+}
 
-	int readIntFromFile(const std::string& filename) {
-		std::ifstream file(filename);
-		int number = 0;
-		if (file.is_open()) {
-			file >> number;
-			file.close();
-			std::cout << "Число успешно считано из файла " << filename << std::endl;
-		}
-		else {
-			std::cout << "Не удалось открыть файл " << filename << std::endl;
-		}
-		return number;
+int readIntFromFile(const std::string& filename) {
+	std::ifstream file(filename);
+	int number = 0;
+	if (file.is_open()) {
+		file >> number;
+		file.close();
 	}
-
-	int GamesPlayed = readIntFromFile("Games.txt");
-	int FirstPlayerWonCount = readIntFromFile("firstWin.txt");
-	int SecondPlayerWonCount = readIntFromFile("secondWin.txt");
-	
+	return number;
+}
 
 
-	void ShowStatistics() {
-		std::cout << "Матчей сыгранно: " << GamesPlayed << endl;
-		std::cout << "Побед первого игрока: " << FirstPlayerWonCount << endl;
-		std::cout << "Побед второго игрока: " << SecondPlayerWonCount << endl;
-	}
-
+void ShowStatistics() {
+	std::cout << "Матчей сыгранно: " << readIntFromFile("Games.txt") << endl;
+	std::cout << "Побед первого игрока: " << readIntFromFile("firstWin.txt") << endl;
+	std::cout << "Побед второго игрока: " << readIntFromFile("secondWin.txt") << endl;
+}
