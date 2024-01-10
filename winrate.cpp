7b5858cbba2bfb0932c9f7f3d20e257c38ea8a70
Pavlovich Ilya <155195533+dealife@users.noreply.github.com>
2024-01-10 14:55:30 +0300

    Add files via upload

---
+++ winrate.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include "Header.h"
+	void writeIntToFile(int number, const std::string& filename) {
+		std::ofstream file(filename, std::ios::trunc);
+		if (file.is_open()) {
+			file << number;
+			file.close();
+			std::cout << "Число успешно записано в файл " << filename << std::endl;
+		}
+		else {
+			std::cout << "Не удалось открыть файл " << filename << std::endl;
+		}
+	}
+
+	int readIntFromFile(const std::string& filename) {
+		std::ifstream file(filename);
+		int number = 0;
+		if (file.is_open()) {
+			file >> number;
+			file.close();
+			std::cout << "Число успешно считано из файла " << filename << std::endl;
+		}
+		else {
+			std::cout << "Не удалось открыть файл " << filename << std::endl;
+		}
+		return number;
+	}
+
+	int GamesPlayed = readIntFromFile("Games.txt");
+	int FirstPlayerWonCount = readIntFromFile("firstWin.txt");
+	int SecondPlayerWonCount = readIntFromFile("secondWin.txt");
+	
+
+
+	void ShowStatistics() {
+		std::cout << "Матчей сыгранно: " << GamesPlayed << endl;
+		std::cout << "Побед первого игрока: " << FirstPlayerWonCount << endl;
+		std::cout << "Побед второго игрока: " << SecondPlayerWonCount << endl;
+	}
+
