2f83a0842dd22a4bcdb1d6b603d937e63c1405de
Pavlovich Ilya <155195533+dealife@users.noreply.github.com>
2024-01-10 14:51:11 +0300

    Add files via upload

---
+++ winrate.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+class WinRateModule {
+private:
+    bool* matches;
+    int size; 
+public:
+
+    WinRateModule(int size) : size(size), matches(new bool[size]) {}
+
+    ~WinRateModule() {
+        delete[] matches;
+    }
+    void addMatch(int index, bool win) {
+        if (index >= 0 && index < size) {
+            matches[index] = win;
+        }
+    }
+    void displayWinRate() {
+        int winCount = 0;
+        for (int i = 0; i < size; i++) {
+            if (matches[i]) {
+                winCount++;
+            }
+        }
+        double winRate = (double)winCount / size;
+        std::cout << "Win rate: " << winRate * 100 << "%" << std::endl;
+    }
+};
