81d947c39423fed29ba45501182ef51560a712e8
Pavlovich Ilya <155195533+dealife@users.noreply.github.com>
2024-01-10 16:05:08 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,10 +1,13 @@
 #pragma once
+#pragma once
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 void board();
 int check_win();
 void writeIntToFile(int number, const std::string& filename);
+int readIntFromFile(const std::string& filename);
 void ShowStatistics();
