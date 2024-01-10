8595d59579c0ba26b785ea5f915cb30688e393da
Pavlovich Ilya <155195533+dealife@users.noreply.github.com>
2024-01-10 14:59:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-﻿#include "Header.h"
-
+#include "Header.h"
+#include <fstream>
 char square[10] = { 'o','1','2','3','4','5','6','7','8','9' };
 int player = 1, i, choice;
 char mark;
+int GamesPlayed, FirstPlayerWonCount, SecondPlayerWonCount;
+std::string filename1 = "firstWin.txt";
+std::string filename2 = "SecondWin.txt";
+std::string filename3 = "Games.txt";
+
 
 int main()
 {
@@ -32,13 +37,19 @@ int main()
 
         i = check_win();
     } while (i == -1);
-
+    writeIntToFile(++GamesPlayed, filename3);
+    if(--player == 1){
+        writeIntToFile(++FirstPlayerWonCount, filename1)
+    }
+    else {
+        writeIntToFile(++SecondPlayerWonCount, filename2)
+    }
+    
     board();
-
     if (i == 1)
         cout << "Игрок " << --player << " победил!" << endl;
     else
         cout << "Это ничья!" << endl;
-
+    ShowStatistics();
     return 0;
 }
