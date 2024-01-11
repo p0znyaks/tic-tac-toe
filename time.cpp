2254235011d4ce37baa42dc88ecf9c0f30ad5e4f
Roman <123767751+roma6kk@users.noreply.github.com>
2024-01-11 10:01:20 +0300

    Обновил time.cpp

--- time.cpp
+++ time.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 auto start = chrono::steady_clock::now();
 
-int time()
+int timeE()
 {
 	auto current = chrono::steady_clock::now();
 	cout << "Время с начала игры: " << chrono::duration_cast<chrono::seconds>(current - start).count() << " секунд.\n";
