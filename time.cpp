3edc366f89c1f17b963e83d1dad8e492ed20f0a2
keko26 <155194806+keko26@users.noreply.github.com>
2024-01-11 08:27:56 +0300

    Update time.cpp

--- time.cpp
+++ time.cpp
@@ -1,6 +1,5 @@
 #include "Header.h"
 #include <chrono>
-#include <thread>
 
 using namespace std;
 
@@ -8,7 +7,6 @@ auto start = chrono::steady_clock::now();
 
 int time()
 {
-	this_thread::sleep_for(std::chrono::seconds(2));
 	auto current = chrono::steady_clock::now();
 	cout << "Время с начала игры: " << chrono::duration_cast<chrono::seconds>(current - start).count() << " секунд.\n";
 	return 0;
